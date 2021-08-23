82d4bc2dccb76c92382dce9727cc757981d8dc52
Cleverson Lira <cleverson_lira@outlook.com>
2021-08-23 02:18:40 -0300

    separando em arquivos

--- js/getOne.js
+++ js/getOne.js
@@ -1,28 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const table = document.querySelector('tbody');
-    const url = new URL("http://localhost:8080/pessoas");
-    const arrayPessoas = new Array();
-    fetch(url)
-    .then(dados => dados.json())
-    .then(resposta => { resposta.content.forEach(pessoa => { 
-            table.innerHTML +=
-            `
-            <tr>
-                <td>${pessoa.id}</td>
-                <td>${pessoa.nome}</td>
-                <td>${pessoa.cpf}</td>
-                <td>${pessoa.sexo}</td>
-                <td>${pessoa.dataNascimento}</td>
-                <td>${pessoa.nacionalidade}</td>
-                <td>${pessoa.naturalidade}</td>    
-                <td class="editar">Editar</td>
-                <td class="excluir">Excluir</td>                            
-            <tr>  
-            `
-        })
-    })
-});
-
 const busca = document.querySelector("#buscar");
 const id = document.querySelector("#id-pessoa");
 busca.addEventListener("click", () => {
